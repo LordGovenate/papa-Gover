09cb7732ccdb163a968f17cb73d2f66dd7f61d26
¨Govenate¨ <LordGovenate>
2025-02-05 01:03:19 -0600

    p2p ingreso carpeta

---
+++ p2p_chat/cmd/chat/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/LordGovenate/p2p_chat/internal/peer"
+)
+
+func main(){
+	
+	operation := os.Args[1]
+	connection := os.Args[2]
+	username := os.Args[3]
+	
+	if operation == "connect" {
+		peer.ConnectToPeer(connection, username)
+	} else {
+		peer.StartListening(connection, username)
+	}
+
+}
---
+++ p2p_chat/go.mod
@@ -0,0 +1,3 @@
+module github.com/LordGovenate/p2p_chat
+
+go 1.23.4
